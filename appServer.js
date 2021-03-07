014108346500ae8cfe64527eeef78892ecf80bdd
joaofavara <joaopedro_favara@hotmail.com>
2021-03-07 09:38:51 -0300

    feat: create new server with cron job and webhooks

--- appServer.js
+++ appServer.js
@@ -1,23 +1,35 @@
+const schedule = require('node-schedule');
+const {
+    timer,
+    repositories,
+    owner,
+    auth,
+    slackWebHook
+} = require('./config');
+const slack = require('./slackIntegration')(slackWebHook);
+const { filterPullRequestInformation } = require('./apiGitHub/utils');
+const gitNotification = require('./apiGitHub')(slack, owner, auth, repositories);
 const express = require('express');
+
+schedule.scheduleJob(timer, function(){
+    const date = new Date()
+    console.log(`Running time: ${date}`);
+    gitNotification();
+});
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    const test = {
-        nome: 'Batata',
-        idade: 12
-    };
-    res.json(test);
-})
-
 app.post('/github', (req, res, next) => {
     const result = req.body;
-    console.log('result: ', result);
-    // return res.status(200);
+    const eventType = req.header['X-GitHub-Event'] === 'pull_request';
+
+    if (eventType) {
+        const data = filterPullRequestInformation(result.pull_request, []);
+        slack(data);
+    }
     return res.end();
-    // return next();
 })
 
 const port = process.env.PORT || 5000;
