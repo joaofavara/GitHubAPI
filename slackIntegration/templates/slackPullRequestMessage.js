9c95909dfa9b95b5adc1a54c3717fc48dba15618
joaofavara <joaopedro_favara@hotmail.com>
2021-03-08 07:50:49 -0300

    refactor: spit slack sender and slack template

---
+++ slackIntegration/templates/slackPullRequestMessage.js
@@ -0,0 +1,37 @@
+module.exports = (pullRequests) => {
+    let pullRequestData = '';
+    let pullRequestsRepositoryName = '';
+    // if (!pullRequest.isDraft) {
+    //     pullRequestsRepositoryName = pullRequest.repositoryName;
+    //     pullRequestData += `<${pullRequest.url}|${pullRequest.title}> (${pullRequest.whoOpened})\n\tAbertura do PR: ${pullRequest.createDate}\n\tAssignees: ${pullRequest.reviewers}\n\n\n`
+    // }
+    pullRequests.forEach((pullRequest) => {
+        if (!pullRequest.isDraft) {
+            pullRequestsRepositoryName = pullRequest.repositoryName;
+            pullRequestData += `<${pullRequest.url}|${pullRequest.title}> (${pullRequest.whoOpened})\n\tAbertura do PR: ${pullRequest.createDate}\n\tAssignees: ${pullRequest.reviewers}\n\n\n`
+        }
+    });
+    
+    const text = {
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": `*Repositorio ${pullRequestsRepositoryName} esta com Pull Requests abertos:*`
+                }
+            },
+            {
+                "type": "section",
+                "block_id": "section567",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": `${pullRequestData}`,
+                },
+            },
+            
+        ]
+    };
+
+    return text;
+}
