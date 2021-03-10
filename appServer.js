9f6d5ac7863a61ec71c092fa3e26c4724d276fa5
joaofavara <joaopedro_favara@hotmail.com>
2021-03-10 07:40:01 -0300

    feat: modify the mtp title and add body to mtp pull request

--- appServer.js
+++ appServer.js
@@ -15,6 +15,7 @@ const express = require('express');
 const slackPullRequestMTP = require('./slackIntegration/templates/slackPullRequestMTP');
 const slackPullRequestMessage = require('./slackIntegration/templates/slackPullRequestMessage');
 const slackRefresh = require('./slackIntegration/templates/slackRefresh');
+const formatDate = require('./apiGitHub/utils/formatDate');
 
 
 schedule.scheduleJob(timer, function(){
@@ -51,28 +52,39 @@ app.post('/refresh', (req, res) => {
 })
 
 app.post('/mtp', async (req, res) => {
-    const parameters = req.body['text'].split(' ');
-    console.log('parameters: ', parameters);
-    const repo = parameters[0];
-    const head = parameters[1];
-    const base = parameters[2];
-
-    const octokit = new Octokit({
-        auth,
-        baseUrl: 'https://api.github.com',
-    });
-
-    const { data } = await octokit.pulls.create({
-        owner,
-        repo,
-        head,
-        base,
-        title: 'MTP'
-      });
-
-    const message = slackPullRequestMTP(data.html_url, repo);
-    await slackSender(message);
-    return res.status(200).end()
+    try {
+        const parameters = req.body['text'].split(' ');
+        console.log('parameters: ', parameters);
+        const repo = parameters[0];
+        const head = parameters[1];
+        const base = parameters[2];
+    
+        const octokit = new Octokit({
+            auth,
+            baseUrl: 'https://api.github.com',
+        });
+    
+        const body = '### Changes\n#### Add\n#### Remove\n#### Refactor\n'
+        const title = `MTP v${formatDate()}`;
+    
+        const { data } = await octokit.pulls.create({
+            owner,
+            repo,
+            head,
+            base,
+            title,
+            body,
+          });
+    
+        const message = slackPullRequestMTP(data.html_url, repo);
+        await slackSender(message);
+        return res.status(200).end()
+    } catch (err) {
+        // const message = slackPullRequestMTP(data.html_url, repo);
+        // await slackSender(message);
+        console.log(err.errors[0].message);
+        return res.status(400).statusMessage(err.errors[0].message)
+    }
 })
 
 const port = process.env.PORT || 5000;
